dbad73d6023f18a42e71d61dc957222bd32ac402
Michael Behringer <Michael.Behringer@hama.de>
2022-01-12 15:44:44 +0100

    added setup_OpenCV_Part1.sh

---
+++ setup_OpenCV_Part1.sh
@@ -0,0 +1,9 @@
+echo starting installation of OpenCV
+sudo apt install cmake build-essential pkg-config git	
+sudo apt install libjpeg-dev libtiff-dev libjasper-dev libpng-dev libwebp-dev libopenexr-dev	
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libdc1394-22-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev	
+sudo apt install libgtk-3-dev python3-pyqt5	
+sudo apt install libatlas-base-dev liblapacke-dev gfortran	
+sudo apt install libhdf5-dev libhdf5-103	
+sudo apt install python3-dev python3-pip python3-numpy	
+sudo nano /etc/dphys-swapfile
