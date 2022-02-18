14285d756a0b222b55f70c94b60ca6cca0fc77ef
Michael Behringer <michabehringer@gmail.com>
2022-02-19 00:29:59 +0100

    final touches, screenshots does not override

--- facePi/main.py
+++ facePi/main.py
@@ -1,137 +1,55 @@
-# USAGE
-# python3 detect_mask_webcam.py
+# facePi
 
-# import the necessary packages
+import argparse
+import time
+import os
+import cv2
+import numpy as np
+import imutils
+import rainbowhat
+from flask import Flask, render_template, Response, request
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 from imutils.video import VideoStream
-from flask import Flask, render_template, Response, request
-import numpy as np
-import argparse
-import imutils
-import time
-import cv2
-import os
-import rainbowhat
 
+#-----------------------------------------------------------------
+PROTOTXT_FILE_PATH = "tensorflow_files/deploy.prototxt"
+MODEL_FILE_PATH = "tensorflow_files/mask_detector.model"
+CAFFEMODEL_FILE_PATH = "tensorflow_files/res10_300x300_ssd_iter_140000.caffemodel"
+SCREENSHOT_EXPORT_PATH = "screenshots/"
+#-----------------------------------------------------------------
+
+app = Flask(__name__)
 save_img = None
 isAlarmActive = True
 isViedoFeedActive = True
 
-def detect_and_predict_mask(frame, faceNet, maskNet, args):
-	# grab the dimensions of the frame and then construct a blob
-	# from it
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-		(104.0, 177.0, 123.0))
+print("DEBUG: loading face detector model")
+faceNet = cv2.dnn.readNet(PROTOTXT_FILE_PATH, CAFFEMODEL_FILE_PATH)
 
-	# pass the blob through the network and obtain the face detections
-	faceNet.setInput(blob)
-	detections = faceNet.forward()
+print("DEBUG: loading face mask detector model")
+maskNet = load_model(MODEL_FILE_PATH)
 
-	# initialize our list of faces, their corresponding locations,
-	# and the list of predictions from our face mask network
-	faces = []
-	locs = []
-	preds = []
+print("DEBUG: activating Camera")
+videoStream = VideoStream(src=0).start()
 
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
-		confidence = detections[0, 0, i, 2]
-
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
-
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
-
-			# add the face and bounding boxes to their respective
-			# lists
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
-
-	# only make a predictions if at least one face was detected
-	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
-		faces = np.array(faces, dtype="float32")
-		preds = maskNet.predict(faces, batch_size=32)
-
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
-	return (locs, preds)
-    
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--face", type=str,
-    default="face_detector",
-    help="path to face detector model directory")
-ap.add_argument("-m", "--model", type=str,
-    default="mask_detector.model",
-    help="path to trained face mask detector model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-    help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
-
-# load our serialized face detector model from disk
-print("[INFO] loading face detector model...")
-prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
-weightsPath = os.path.sep.join([args["face"],
-    "res10_300x300_ssd_iter_140000.caffemodel"])
-faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
-
-# load the face mask detector model from disk
-print("[INFO] loading face mask detector model...")
-maskNet = load_model(args["model"])
-
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
-
-# construct the argument parser and parse the arguments
-def myTest():
-	# loop over the frames from the video stream
+def mainVideoFeedLoop():
+	global isViedoFeedActive
+	global save_img
+	global isAlarmActive
 	while True:
-		# grab the frame from the threaded video stream and resize it
-		# to have a maximum width of 400 pixels
-		frame = vs.read()
+		frame = videoStream.read()
 		frame = cv2.flip(frame, 0)
 		frame = imutils.resize(frame, width=500)
+		(boxes, predictions) = detect_mask(frame, faceNet, maskNet)
 
-		# detect faces in the frame and determine if they are wearing a
-		# face mask or not
-		(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet, args)
-
-		# loop over the detected face locations and their corresponding
-		# locations
 		withCounter = 0
 		withoutCounter = 0
-		for (box, pred) in zip(locs, preds):
-			# unpack the bounding box and predictions
+		for (box, prediction) in zip(boxes, predictions):
 			(startX, startY, endX, endY) = box
-			(mask, withoutMask) = pred
+			(mask, withoutMask) = prediction
 
-			# determine the class label and color we'll use to draw
-			# the bounding box and text
 			if mask > withoutMask:
 				label = "Thank You. Mask On."
 				color = (0, 255, 0)
@@ -140,77 +58,104 @@ def myTest():
 			else:
 				label = "No Face Mask Detected"
 				color = (0, 0, 255)
-				global isAlarmActive
 				withoutCounter+=1
 				if(isAlarmActive and isViedoFeedActive):
 					rainbowhat.buzzer.midi_note(60, 1)
-			
-			# display the label and bounding box rectangle on the output
-			# frame
-			cv2.putText(frame, label, (startX-50, startY - 10),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+
+			cv2.putText(frame, label, (startX-50, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
 			cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-		# show the output frame
-		#global RGB_img
-		RGB_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		RGB_IMG = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
 		if(not isViedoFeedActive):
-			RGB_img = cv2.imread('./static/logo.jpeg', 0)
+			RGB_IMG = cv2.imread('./static/logo.jpeg', 0) #return logo anstatt kamerabild
 			rainbowhat.display.print_str('----')
 		else:
 			rainbowhat.display.print_str('0'+str(withCounter)+'0'+str(withoutCounter))
-		global save_img
-		save_img = RGB_img
-		retValue, image = cv2.imencode('.jpg', RGB_img)
-		
+
+		save_img = RGB_IMG
+		retValue, image = cv2.imencode('.jpg', RGB_IMG)
+
 		rainbowhat.display.show()
 		yield(decodeData(image.tobytes()))
 		key = cv2.waitKey(1) & 0xFF
 
-		# if the `q` key was pressed, break from the loop
 		if key == ord("q"):
 			break
-	# do a bit of cleanup
 	cv2.destroyAllWindows()
-	vs.stop()
+	videoStream.stop()
 
-app = Flask(__name__)
+def detect_mask(frame, faceNet, maskNet):
+	faces = []
+	boxes = []
+	predictions = []
+
+	(height, width) = frame.shape[:2] #länge breite von Bild ermitteln
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0)) #blob aus Bild generieren (Binary Large Objects)
+
+	faceNet.setInput(blob)
+	detections = faceNet.forward() #Neurales Netzt aus blob masken erkennen
+
+	for i in range(0, detections.shape[2]):
+		confidence = detections[0, 0, i, 2] #Prozentangebe wie sicher sich das Neurale Netzt ist eine Maske erkannt zu haben
+
+		if confidence > 0.5:
+			box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(width - 1, endX), min(height - 1, endY))
+
+			face = frame[startY:endY, startX:endX]
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+			face = cv2.resize(face, (224, 224))
+			face = img_to_array(face)
+			face = preprocess_input(face)
+
+			faces.append(face)
+			boxes.append((startX, startY, endX, endY))
+
+	if len(faces) > 0: #wenn mindestesns 1 Gesicht gefunden wurde
+		faces = np.array(faces, dtype="float32")
+		predictions = maskNet.predict(faces, batch_size=32) #(　’ ‘)ﾉﾉ⌒●~*
+
+	return (boxes, predictions)
 
 @app.route('/')
 def loadHTML():
-    return render_template('index.html')
-
+	return render_template('index.html')
 
 def decodeData(imageBytes):
-    return (b'--frame\r\n Content-Type: image/jpeg\r\n\r\n' + imageBytes + b'\r\n\r\n')
+	return (b'--frame\r\n Content-Type: image/jpeg\r\n\r\n' + imageBytes + b'\r\n\r\n')
 
 @app.route('/alarm')
 def alarm():
-    global isAlarmActive
-    isAlarmActive = not isAlarmActive
-    return 'alarm'
+	global isAlarmActive
+	isAlarmActive = not isAlarmActive
+	return 'alarm'
 
 @app.route('/picture')
 def picture():
-    global save_img
-    cv2.imwrite('hallo.jpg', save_img)
-    return 'picture'
+	global save_img
+	timestr = time.strftime("%Y%m%d_%H%M%S")
+	cv2.imwrite(SCREENSHOT_EXPORT_PATH + timestr + '_screenshot.jpg', save_img)
+	return 'picture'
 
 @app.route('/activateViedoFeed')
 def activateViedoFeed():
-    global isViedoFeedActive
-    isViedoFeedActive = True
-    return 'activateViedoFeed'
+	global isViedoFeedActive
+	isViedoFeedActive = True
+	return 'activateViedoFeed'
 
 @app.route('/disableViedoFeed')
 def disableViedoFeed():
-    global isViedoFeedActive
-    isViedoFeedActive = False
-    return 'disableViedoFeed'
+	global isViedoFeedActive
+	isViedoFeedActive = False
+	return 'disableViedoFeed'
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(myTest(), mimetype='multipart/x-mixed-replace; boundary=frame')
+	return Response(mainVideoFeedLoop(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+	app.run(host='0.0.0.0', debug=False)
